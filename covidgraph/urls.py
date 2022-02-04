2a85646ef674ef43a6038d2264a4174a567dbfbd
19127592 <lmtri19@clc.fitus.edu.vn>
2022-02-04 22:00:53 +0700

    building features

--- covidgraph/urls.py
+++ covidgraph/urls.py
@@ -2,9 +2,17 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.home),
-    path('test-page', views.test),
-    path('user-manual',views.user_manual),
-    path('register',views.register),
-    path('search',views.search),
+    path('', views.home,name='homepage'),
+
+    path('test-page', views.test,name='test'),
+    path('test-page/count-words',views.count,name='count'),
+
+    path('user-manual',views.user_manual,name='manual'),
+    path('register-person',views.register,name='register'),
+    path('search',views.search,name='search'),
+
+    path('login-account',views.login,name='login'),
+    path('signup-account',views.signup,name='signup'),
+    
+    path('about-us',views.about,name='about')
 ]
--- covidgraph/views.py
+++ covidgraph/views.py
@@ -1,18 +1,43 @@
 from urllib import request
-from django.http import HttpResponse
 from django.shortcuts import render
 
+from collections import Counter
+
 def home(request):
-    return render(request,'mainpage/home.html')
+    return render(request,'mainpage/homepage/home.html')
+
+def about(request):
+    return render(request,'mainpage/about/about.html')
 
 def user_manual(request):
-    return render(request,'manual/manual.html')
+    return render(request,'mainpage/manual/manual.html')
 
 def register(request):
-    return render(request,'register/manual.html')
+    return render(request,'mainpage/register/register.html')
 
 def search(request):
-    return render(request,'search/search.html')
+    return render(request,'mainpage/search/search.html')
+
+def login(request):
+    return render(request,'mainpage/auth/login.html')
+
+def signup(request):
+    return render(request,'mainpage/auth/signup.html')
 
 def test(request):
-    return HttpResponse('Test page')
+    return render(request,'mainpage/test/test.html')
+
+def count(request):
+    fulltext = request.GET['fulltext']
+    countwords = fulltext.split()
+    worddict = dict()
+    for word in countwords:
+        if word in worddict:
+            worddict[word]+= 1
+        else:
+            worddict[word] = 1
+    max_value = max(worddict.values())
+    
+    most_frequent = [(word,value) for word,value in worddict.items() if value==max_value]
+
+    return render(request,'mainpage/test/count.html',{'fulltext':fulltext,'count':len(countwords),'most_frequent':most_frequent})
